e80c217529434364c3b8af1c88ae6228021aa6f5
ziyuanli <kyli223@yeah.net>
2024-06-25 14:16:02 -0400

    Test

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
 import sys
 # Get the name from the command-line argument, which is in the second position of the command-line argument
 name = sys.argv[1]
-print("Hello"+ name)
+print("Hello "+ name)
